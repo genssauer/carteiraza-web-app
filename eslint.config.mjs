3b03cf929b96e36e3cf10b6cdb6335d6dbc9e209
Genesson <genesson_sauer@hotmail.com>
2025-03-01 16:25:36 -0300

    initial: set up project structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import nx from '@nx/eslint-plugin';
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
+  eslintPluginPrettierRecommended,
   {
     ignores: ['**/dist'],
   },
@@ -26,16 +28,7 @@ export default [
     },
   },
   {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.cts',
-      '**/*.mts',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
+    files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts', '**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
     // Override or add rules here
     rules: {},
   },
