6f2e76205011bb922a8086c1f4ff79b591abcb62
Genesson <genesson_sauer@hotmail.com>
2025-03-10 20:54:55 -0300

    feat: add routing for base layout and institutional pages

--- apps/web-app/src/app/app.routes.ts
+++ apps/web-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () => import('./core/layout/base/base.layout').then(m => m.BaseLayout),
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./domain/institutional/routes/institutional.routes').then(m => m.INSTITUTIONAL_ROUTES),
+      },
+    ],
+  },
+];
