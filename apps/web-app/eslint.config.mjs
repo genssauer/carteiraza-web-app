65f1e258a2cb6be97472bead43d446a5306c521c
Genesson <genesson_sauer@hotmail.com>
2025-03-10 19:44:25 -0300

    refactor: rename app prefix to 'cza' and update component structure

--- apps/web-app/eslint.config.mjs
+++ apps/web-app/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'cza',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'cza',
           style: 'kebab-case',
         },
       ],
