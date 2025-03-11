c8148cb461e57ed5135be16f6e6e2b99144df702
Genesson <genesson_sauer@hotmail.com>
2025-03-10 21:16:51 -0300

    feat: add institutional page translations and language switcher for contact page

--- apps/web-app/src/app/domain/institutional/pages/contact/contact.page.ts
+++ apps/web-app/src/app/domain/institutional/pages/contact/contact.page.ts
@@ -1,10 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'cza-contact',
-  imports: [],
+  imports: [TranslocoModule, NzButtonComponent],
   templateUrl: './contact.page.html',
   styleUrl: './contact.page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactPage {}
+export class ContactPage {
+  private translocoService = inject(TranslocoService);
+
+  protected changeLang(lang: string) {
+    this.translocoService.setActiveLang(lang);
+  }
+}
